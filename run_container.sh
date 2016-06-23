f7c5279b54604fe0d960fd69503debb1d493c830
Gerard Braad <me@gbraad.nl>
2016-06-23 08:48:42 +0000

    Installation script

--- run_container.sh
+++ run_container.sh
@@ -3,27 +3,6 @@
 # Doing this in a separate script lets us do it step by step while using a
 # single docker layer.
 
-# Install tools
-dnf remove -y vim-minimal
-dnf install -y \
-    sudo \
-    git \
-    ansible \
-    python-pip \
-    python2-dnf \
-    htop
-
-# Install Oh-my-zsh
-# curl -sSL http://install.ohmyz.sh | sh
-
-# Add personal user
-adduser gbraad
-echo "gbraad ALL=(root) NOPASSWD:ALL" | tee -a /etc/sudoers.d/gbraad
-chmod 0440 /etc/sudoers.d/gbraad
-
-# Install personal dotfiles
-curl -sSL https://raw.githubusercontent.com/gbraad/dotfiles/master/install.yml -o /tmp/install.yml
-su - gbraad -c "ansible-playbook /tmp/install.yml"
-
-# Clean up
-dnf clean all
+# Installation script
+curl -sSL https://raw.githubusercontent.com/gbraad/devenv/master/install.sh -o /tmp/install.sh
+bash /tmp/install.sh
