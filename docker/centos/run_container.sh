b381724b27108b84d15072fe7066916e28d636ed
Gerard Braad <me@gbraad.nl>
2016-07-23 13:51:51 +0000

    Temporary CentOS script

--- docker/centos/run_container.sh
+++ docker/centos/run_container.sh
@@ -3,6 +3,27 @@
 # Doing this in a separate script lets us do it step by step while using a
 # single docker layer.
 
-# Installation script
-curl -sSL https://raw.githubusercontent.com/gbraad/devenv/master/scripts/install.sh -o /tmp/install.sh
-bash /tmp/install.sh
+# Install tools
+yum remove -y vim-minimal
+yum install -y \
+    sudo \
+    git \
+    ansible \
+    python-pip \
+    python2-dnf \
+    htop
+
+# Install Oh-my-zsh
+# curl -sSL http://install.ohmyz.sh | sh
+
+# Add personal user
+adduser gbraad
+echo "gbraad ALL=(root) NOPASSWD:ALL" | tee -a /etc/sudoers.d/gbraad
+chmod 0440 /etc/sudoers.d/gbraad
+
+# Install personal dotfiles
+curl -sSL https://raw.githubusercontent.com/gbraad/dotfiles/master/install.yml -o /tmp/install.yml
+su - gbraad -c "ansible-playbook /tmp/install.yml"
+
+# Clean up
+yum clean all
