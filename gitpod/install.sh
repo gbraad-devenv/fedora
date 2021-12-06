deb024fa1e2e8e686ef2aae0602256f49e4690dd
Gerard Braad <me@gbraad.nl>
2021-12-06 21:58:31 +0800

    Move folder

--- gitpod/install.sh
+++ gitpod/install.sh
@@ -3,6 +3,8 @@ rm -rf /workspace/devenv
 mkdir ~/Projects
 ln -s ~/Projects /workspace/devenv
 
+cd ~
+
 if [ ! -d "~/.dotfiles" ]; then
     curl -sSL https://raw.githubusercontent.com/gbraad/dotfiles/master/install.sh -o /tmp/install.sh &&
     sh /tmp/install.sh &&
