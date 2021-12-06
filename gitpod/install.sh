cdf06d101a0c04e231fa722e2a6e12eafdadf8ed
Gerard Braad <me@gbraad.nl>
2021-12-06 21:54:32 +0800

    Change order and move workspace

--- gitpod/install.sh
+++ gitpod/install.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
+rm -rf /workspace/devenv
+mkdir ~/Projects
+ln -s ~/Projects /workspace/devenv
+
 if [ ! -d "~/.dotfiles" ]; then
     curl -sSL https://raw.githubusercontent.com/gbraad/dotfiles/master/install.sh -o /tmp/install.sh &&
     sh /tmp/install.sh &&
     mv ~/.bashrc-nochsh ~/.bashrc 
 fi
 
-rm -rf /workspace/devenv
-
-ln -s /workspace/devenv/ ~/Projects
